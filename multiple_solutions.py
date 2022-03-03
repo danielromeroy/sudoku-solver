985e369970a9987ff3579230205fa24928a52b94
danielromeroy <s220260@student.dtu.dk>
2022-03-03 02:17:40 +0100

    Added support for multiple solutions.

--- multiple_solutions.py
+++ multiple_solutions.py
@@ -1,7 +1,8 @@
 import itertools as itt
+import copy as c
 
-INPUT_FILE = "./data/wikipedia_sudoku"
-MAX_SUDOKUS = 10000
+INPUT_FILE = "./data/wikipedia_sudoku_2"
+MAX_SUDOKUS = 500000
 
 
 class SudokuSolver:
@@ -30,9 +31,10 @@ class SudokuSolver:
                     nums_in_subsq = []
                     for k in range(1, 10):  # row
                         for l in range(1, 10):  # col
+                            # Check if current row and col are in the same subsquare
                             if ((i - 1) // 3) == ((k - 1) // 3) and \
-                               ((j - 1) // 3) == ((l - 1) // 3) and \
-                               self.solve_status[k - 1][l - 1] != "X":
+                                    ((j - 1) // 3) == ((l - 1) // 3) and \
+                                    self.solve_status[k - 1][l - 1] != "X":
                                 nums_in_subsq.append(self.solve_status[k - 1][l - 1])
 
                     forbidden_nums = list(set(nums_in_row + nums_in_col + nums_in_subsq))
@@ -42,12 +44,10 @@ class SudokuSolver:
                     self.possible_nums[f"{i}{j}"] = cell_possible_nums
 
     def has_unique_solutions(self):
-        has_unique = False
         for item in self.possible_nums.items():
             if len(item[1]) == 1:
-                has_unique = True
-                break
-        return has_unique
+                return True
+        return False
 
     def assign_unique_values(self):
         for i in range(1, 10):  # row
@@ -59,8 +59,15 @@ class SudokuSolver:
             self.solved = True
 
     def explode(self):
-        # possible_solutions = []
-        pass
+        possible_solutions = []
+        for i in range(1, 10):  # row
+            for j in range(1, 10):  # col
+                if self.solve_status[i - 1][j - 1] == "X":
+                    for num in self.possible_nums[f"{i}{j}"]:
+                        new_solution = c.deepcopy(self.solve_status)
+                        new_solution[i - 1][j - 1] = num
+                        possible_solutions.append(new_solution)
+        return possible_solutions
 
     def print_string(self, n):
         print(f"Solution number {n}:")
@@ -77,13 +84,35 @@ class SudokuSolver:
         print(sudoku_string)
 
 
+def all_solved():
+    global solvers
+    for solver in solvers:
+        if not solver.solved:
+            return False
+    return True
+
+
+
 with open(INPUT_FILE) as sudoku_file:
     init_sudoku_solve_status = [line.split() for line in sudoku_file]
 
-solver = SudokuSolver(init_sudoku_solve_status)
+solvers = [SudokuSolver(init_sudoku_solve_status)]
 
-while not solver.solved:
-    solver.compute_possible_numbers()
-    solver.assign_unique_values()
+debug_count = 0
+while not all_solved():
+    debug_count += 1
+    print(f"{debug_count=} {len(solvers)=}")
+    for i in reversed(range(0, len(solvers))):
+        solver = solvers[i]
+        if not solver.solved:
+            # if len(solvers) > MAX_SUDOKUS:
+            #     raise MemoryError("Amount of sudokus exceeded limit.")
+            solver.compute_possible_numbers()
+            if solver.has_unique_solutions():
+                solver.assign_unique_values()
+            else:
+                solvers.pop(i)
+                solvers.extend([SudokuSolver(status) for status in solver.explode()])
 
-solver.print_string(1)
+for i in range(len(solvers)):
+    solvers[i].print_string(i + 1)
